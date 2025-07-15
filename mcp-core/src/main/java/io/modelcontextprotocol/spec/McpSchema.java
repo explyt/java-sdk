725cdab0278c9a4908a597322a2b6242027be780
Max Nigmatulin <40598909+emnigma@users.noreply.github.com>
2025-07-15 16:14:40 +0300

    ci: GitHub release and better version management (#1-#6)
    ci: Add gh release button (#1)
    
    ci: No tests (#2)
    
    ci: Add dependencyManagement (#3)
    
    add liquid version (#4)
    
    minor ci
    
    fix: Minor ci, ignore keys (#6)
    
    * minor ci, ignore keys
    
    * try docker + default revision
    
    forgot

--- mcp-core/src/main/java/io/modelcontextprotocol/spec/McpSchema.java
+++ mcp-core/src/main/java/io/modelcontextprotocol/spec/McpSchema.java
@@ -542,6 +542,7 @@ public final class McpSchema {
 		 * Present if the server supports argument autocompletion suggestions.
 		 */
 		@JsonInclude(JsonInclude.Include.NON_ABSENT)
+		@JsonIgnoreProperties(ignoreUnknown = true)
 		public record CompletionCapabilities() {
 		}
 
@@ -549,6 +550,7 @@ public final class McpSchema {
 		 * Present if the server supports sending log messages to the client.
 		 */
 		@JsonInclude(JsonInclude.Include.NON_ABSENT)
+		@JsonIgnoreProperties(ignoreUnknown = true)
 		public record LoggingCapabilities() {
 		}
 
@@ -559,6 +561,7 @@ public final class McpSchema {
 		 * the prompt list
 		 */
 		@JsonInclude(JsonInclude.Include.NON_ABSENT)
+		@JsonIgnoreProperties(ignoreUnknown = true)
 		public record PromptCapabilities(@JsonProperty("listChanged") Boolean listChanged) {
 		}
 
@@ -570,6 +573,7 @@ public final class McpSchema {
 		 * the resource list
 		 */
 		@JsonInclude(JsonInclude.Include.NON_ABSENT)
+		@JsonIgnoreProperties(ignoreUnknown = true)
 		public record ResourceCapabilities(@JsonProperty("subscribe") Boolean subscribe,
 				@JsonProperty("listChanged") Boolean listChanged) {
 		}
@@ -581,6 +585,7 @@ public final class McpSchema {
 		 * the tool list
 		 */
 		@JsonInclude(JsonInclude.Include.NON_ABSENT)
+		@JsonIgnoreProperties(ignoreUnknown = true)
 		public record ToolCapabilities(@JsonProperty("listChanged") Boolean listChanged) {
 		}
 
